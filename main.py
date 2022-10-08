257adfc45d38c612d1f24fd92e1617a2f007f78c
x-actly <80623408+x-actly@users.noreply.github.com>
2022-10-08 18:26:37 +0200

    Update main.py
    reworked script for better performance...

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-import keyboard
-import functions
+import keyboard, pyautogui, random, time, os
 
 
-# example script to loop the afk macro
+def simple_afk_script():
 
-print("press space... and switch back into game\n")
+    ability_list = ['q','w','e','r','s','d','f']
+    item_list = []
+
+    for x in ability_list:
+
+        break_time = random.uniform(1, 3)
+
+        pyautogui.press(x)
+        time.sleep(break_time)
+        print("random time between inputs in seconds: ", break_time,"\n")
+
+    return
+
+
+
+
+print("press space... switch back into game\n")
 
 if keyboard.read_key() == "space":
 
     loop_counter = 0
+    time.sleep(5)
 
     while True:
 
         loop_counter = loop_counter + 1
-        print("loop counter: ", loop_counter,"\n")
+        print("loop counter: ", loop_counter, "\n")
+        simple_afk_script()
 
-        functions.simple_afk_script()
-
-
+        """
+        if loop_counter == 500:
+            os.system("shutdown /s /t 1")
+            break
+        """
 
 else:
     print("wrong key...")
